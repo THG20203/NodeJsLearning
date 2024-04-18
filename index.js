b97397b7446770c782fe875b404b346a99777403
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-18 12:46:53 +0100

    file written

--- index.js
+++ index.js
@@ -14,4 +14,9 @@ const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
 /* WRITE TO FILES */
+/* first write out variable then output it. */
+const textOut = `This is what we know about the avocado: ${textIn}. Created on ${Date.now()}`;
+/* path to file and second what we want to write into that file */
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written");
 
