014a8c8ce72b58a237700cf5403e53457fa14186
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-18 13:13:16 +0100

    going through index.js node asynchronous file

--- index.js
+++ index.js
@@ -6,17 +6,33 @@ core Node.js module providing a lot of useful functionality to access
 and interact with the file system. */
 const fs = require("fs");
 
+
+/* BLOCKING, SYNCHRONOUS WAY */
+
 /* READING FILES */
 /* takes two arguments, first path of file we're reading, also 
 the character in coding */
 /* reading data from file, returning it to us, save it in textIn variable */
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+/* const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
 /* WRITE TO FILES */
 /* first write out variable then output it. */
-const textOut = `This is what we know about the avocado: ${textIn}. Created on ${Date.now()}`;
+/* const textOut = `This is what we know about the avocado: ${textIn}. Created on ${Date.now()}`;
 /* path to file and second what we want to write into that file */
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written");
+/* fs.writeFileSync("./txt/output.txt", textOut); */
+/* console.log("File written"); */
 
+/* NON BLOCKING, ASYNCHRONOUS WAY */
+/* reading file */
+/* again, pass in path to file, file encoding = second argument 
+pass in call back function as second argument. */
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+    /* now going to read the content of read this txt */
+    fs.readFile("./txt/${data1}.txt", "utf-8", (err, data2) => {
+        console.log(data2);
+    })
+})
+/* node js start reading file in the background, will not block code,
+will move on to the next line of code. */
+console.log("will read file");
