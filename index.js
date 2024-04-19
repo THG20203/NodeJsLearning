089accc370914cf807982ce274ee2345413ef561
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-19 11:36:48 +0100

    change output variable

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ const replaceTemplate = (temp, product) => {
     /* let output, replace product name in temp, so from now on manipulate let output */
     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, product.image);
 }
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
